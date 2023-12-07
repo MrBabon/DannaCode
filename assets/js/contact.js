c34a8a58a8ca4781257637d2e37f762f2654c5ad
MrBabon <christophedanna06@gmail.com>
2023-12-07 22:54:36 +0100

    Form de contact fonctionne

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,74 +1,49 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     document.querySelectorAll(".navbar a, footer a").forEach(function (link) {
-//         link.addEventListener("click", function (event) {
-//             event.preventDefault();
-//             var hash = this.hash;
-//             document.querySelector(hash).scrollIntoView({ behavior: "smooth" });
-//         });
-//     });
-
-//     document.getElementById('contact-form').addEventListener('submit', function (e) {
-//         e.preventDefault();
-//         document.querySelectorAll('.comments').forEach(function (comment) {
-//             comment.innerHTML = '';
-//         });
-
-//         var formData = new FormData(this);
-
-//         fetch('../../php/contact.php', {
-//             method: 'POST',
-//             body: formData
-//         })
-//         .then(response => response.json())
-//         .then(result => {
-//             if (result.isSuccess) {
-//                 document.getElementById('contact-form').innerHTML += "<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté.</p>";
-//                 document.getElementById('contact-form').reset();
-//             } else {
-//                 document.getElementById('firstname + .comments').innerHTML = result.firstnameError;
-//                 document.getElementById('name + .comments').innerHTML = result.nameError;
-//                 document.getElementById('email + .comments').innerHTML = result.emailError;
-//                 document.getElementById('phone + .comments').innerHTML = result.phoneError;
-//                 document.getElementById('message + .comments').innerHTML = result.messageError;
-//             }
-//         })
-//         .catch(error => {
-//             console.error("Une erreur s'est produite lors de la soumission du formulaire:", error);
-//         });
-//     });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
-    const formulaireContact = document.getElementById("contact-form");
 
-    formulaireContact.addEventListener("submit", function (event) {
-        event.preventDefault();
+  document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    clearComments();
+    var postdata = new FormData(this);
 
-        // Obtenir les données du formulaire
-        const formData = new FormData(formulaireContact);
-
-        // Envoyer les données du formulaire au script PHP en utilisant AJAX
-        fetch("../php/contact.php", {
-            method: "POST",
-            body: formData,
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            // Gérer la réponse du script PHP
-            if (data.isSuccess) {
-                document.getElementById('contact-form').innerHTML += "<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté.</p>";
-                document.getElementById('contact-form').reset();
-            } else {
-                document.getElementById('firstname + .comments').innerHTML = result.firstnameError;
-                document.getElementById('name + .comments').innerHTML = result.nameError;
-                document.getElementById('email + .comments').innerHTML = result.emailError;
-                document.getElementById('phone + .comments').innerHTML = result.phoneError;
-                document.getElementById('message + .comments').innerHTML = result.messageError;
-            }
-        })
-        .catch(error => {
-            console.error("Error sending form data:", error);
-        });
-    });
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '../php/contact.php', true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        console.log(xhr.responseText); // Ajoutez cette ligne pour voir la réponse dans la console
+        if (xhr.status == 200) {
+          var result = JSON.parse(xhr.responseText);
+          handleFormResponse(result);
+        }
+      }
+    }; // <-- Correction : l'accolade fermante était mal placée
+    xhr.send(postdata);
+  });
 });
+
+
+function clearComments() {
+  var comments = document.querySelectorAll('.comments');
+  comments.forEach(function (comment) {
+    comment.innerHTML = '';
+  });
+}
+
+function handleFormResponse(result) {
+  if (result.isSuccess) {
+    // Modification du message de succès
+    document.getElementById('overlay').style.display = 'flex';
+    document.getElementById('modal').style.display = 'block';
+  } else {
+    // Affichage des messages d'erreur
+    document.getElementById('firstname + .comments').innerHTML = result.firstnameError;
+    document.getElementById('name + .comments').innerHTML = result.nameError;
+    document.getElementById('email + .comments').innerHTML = result.emailError;
+    document.getElementById('phone + .comments').innerHTML = result.phoneError;
+    document.getElementById('message + .comments').innerHTML = result.messageError;
+  }
+}
+function closeModal() {
+  // Fermer la fenêtre modale
+  document.getElementById('overlay').style.display = 'none';
+  document.getElementById('modal').style.display = 'none';
+}
--- php/contact.php
+++ php/contact.php
@@ -1,8 +1,9 @@
 <?php
+    header('Content-Type: application/json');
     $array = array("firstname" => "", "name" => "", "email" => "", "phone" => "", "message" => "", "firstnameError" => "", "nameError" => "", "emailError" => "", "phoneError" => "", "messageError" => "", "isSuccess" => false);
 
     $emailTo = "contact@dannacode.com";
-
+    
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
         $array["firstname"] = verifyInput($_POST["firstname"]);
@@ -20,7 +21,7 @@
 
         }
         else
-            $emailText .= "Firstname:{$array["firstname"]}\n";
+            $emailText .= "Prénom:{$array["firstname"]}\n";
 
         if(empty($array["name"]))
         {
@@ -29,7 +30,7 @@
             
         }
         else
-            $emailText .= "Name: {$array["name"]}\n";
+            $emailText .= "Nom: {$array["name"]}\n";
 
         if(!isEmail($array["email"]))
         {
@@ -46,7 +47,7 @@
             $array["isSuccess"] = false;
         }
         else
-        $emailText .= "Phone: {$array["phone"]}\n";
+        $emailText .= "Téléphone: {$array["phone"]}\n";
 
         if(empty($array["message"]))
         {
@@ -61,32 +62,19 @@
 
         
         
-            if ($array["isSuccess"]) {
-                // Construire le corps du message
-                $emailText .= "Firstname: {$array["firstname"]}\n";
-                $emailText .= "Name: {$array["name"]}\n";
-                $emailText .= "Email: {$array["email"]}\n";
-                $emailText .= "Phone: {$array["phone"]}\n";
-                $emailText .= "Message: {$array["message"]}\n";
-        
-                // En-têtes du courriel
-                $headers = "From: {$array["firstname"]} {$array["name"]} <{$array["email"]}>\r\n";
-                $headers .= "Reply-To: {$array["email"]}\r\n";
-                $headers .= "X-Originating-IP: {$_SERVER['REMOTE_ADDR']}";
-        
-                // Envoyer l'e-mail
-                if (mail($emailTo, "Un message de votre site www.dannacode.com", $emailText, $headers)) {
-                    // Réponse JSON indiquant le succès
-                    $array["isSuccess"] = true;
-                } else {
-                    // Erreur d'envoi d'e-mail
-                    $array["isSuccess"] = false;
-                    $array["errorMessage"] = "Erreur lors de l'envoi de l'e-mail.";
-                }
-            }
-        
-            echo json_encode($array);
+        if($array["isSuccess"])
+        {
+            $headers = "From: {$array["firstname"]} {$array["name"]} <{$array["email"]}>\r\nReply-To: {$array["email"]}\r\nX-Originating-IP: {$_SERVER['REMOTE_ADDR']}";
+            mail($emailTo,"Un message de votre site www.dannacode.com", $emailText , $headers);
+            
+
         }
+        
+
+        echo json_encode($array);
+        
+
+    }
     
     function isPhone($var)
     {
@@ -98,7 +86,6 @@
         return filter_var($var, FILTER_VALIDATE_EMAIL);
 
     }
-
     // POUR SECURISER LE SITE
 
     function verifyInput($var)
